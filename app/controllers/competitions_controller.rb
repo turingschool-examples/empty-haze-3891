89c7e82d436fbe9c58d162ee788cbcbbdccd4e42
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-04-26 08:42:08 -0700

    Add: Competition Index Page with Links to each Competition Show Page

---
+++ app/controllers/competitions_controller.rb
@@ -0,0 +1,11 @@
+class CompetitionsController < ApplicationController
+
+  def index
+    @competitions = Competition.all
+  end
+
+  def show
+
+  end
+
+end
--- spec/features/competitions/index_spec.rb
+++ spec/features/competitions/index_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Competition Index Page' do
   describe 'Competition Index' do
     it 'displays names of all competitions as links to show page' do
-      competition_1 = (name: 'Flight Fest', location: 'San Diego', sport: 'Basketball')
-      competition_2 = (name: 'Jump City', location: 'Brooklyn', sport: 'Basketball')
-      competition_3 = (name: 'Hot Shot', location: 'Tampa', sport: 'Soccer')
+      competition_1 = Competition.create!(name: 'Flight Fest', location: 'San Diego', sport: 'Basketball')
+      competition_2 = Competition.create!(name: 'Jump City', location: 'Brooklyn', sport: 'Basketball')
+      competition_3 = Competition.create!(name: 'Hot Shot', location: 'Tampa', sport: 'Soccer')
 
       visit '/competitions'
       click_link 'Flight Fest'
