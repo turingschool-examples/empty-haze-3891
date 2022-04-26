491581d02f3911bc90ee85e751a52f7438acf19d
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-26 09:43:37 -0600

    testing: validation and association tests all pass

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,2 +1,8 @@
 class Competition < ApplicationRecord
+  has_many :team_comps
+  has_many :teams, through: :team_comps
+
+  validates_presence_of :name
+  validates_presence_of :location
+  validates_presence_of :sport
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,8 @@
 class Team < ApplicationRecord
   has_many :players
+  has_many :team_comps
+  has_many :competitions, through: :team_comps
+
+  validates_presence_of :hometown
+  validates_presence_of :nickname
 end
