c3a15c556a73001a024476c70ab57905956c58e3
Tyler Ross <tross020890@gmail.com>
2022-04-26 10:32:24 -0500

    Feat: created migrations, tested relationships

---
+++ app/models/competition_team.rb
@@ -0,0 +1,5 @@
+class CompetitionTeam < ApplicationRecord
+  belongs_to :team
+  belongs_to :competition
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_215532) do
+ActiveRecord::Schema.define(version: 2022_04_26_151314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "competition_teams", force: :cascade do |t|
+    t.bigint "competition_id"
+    t.bigint "team_id"
+    t.index ["competition_id"], name: "index_competition_teams_on_competition_id"
+    t.index ["team_id"], name: "index_competition_teams_on_team_id"
+  end
+
+  create_table "competitions", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "sport"
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "name"
@@ -31,5 +44,7 @@ ActiveRecord::Schema.define(version: 2022_01_15_215532) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "competition_teams", "competitions"
+  add_foreign_key "competition_teams", "teams"
   add_foreign_key "players", "teams"
 end
