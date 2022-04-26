7d81af7da44e58cc6504d2815606bfaa0e21b4c6
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-26 10:33:30 -0600

    pk

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 get "/competitions", to: "competitions#index"
-get "/competitions/:id", to: "competition#show"
+get "/competitions/:id", to: "competitions#show"
 
 get "/teams", to: "teams#index"
 
--- spec/features/competition/index_spec.rb
+++ spec/features/competition/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Competition Index', type: :feature do
     @comp2 = Competition.create!(name: "Semi_Finals", location: "City2", sport: "Turtle_Fights")
     @comp3 = Competition.create!(name: "Finals_Retake", location: "City3", sport: "High_Pressure_Pressurizing")
 
-  visit "/competition"
+  visit "/competitions"
   end
 
   it 'shows the names of all competitions as links to their resective show page' do
@@ -15,13 +15,13 @@ RSpec.describe 'Competition Index', type: :feature do
        expect(current_path).to eq("/comp1/#{@comp1.id}")
      end
 
-       visit "/competition"
+       visit "/competitions"
     within("#competitions #{@comp2.id}") do
       click_link @comp2.name
        expect(current_path).to eq("/comp2/#{@comp2.id}")
      end
 
-       visit "/competition"
+       visit "/competitions"
     within("#competitions #{@comp3.id}") do
       click_link @comp3.name
        expect(current_path).to eq("/comp3/#{@comp3.id}")
