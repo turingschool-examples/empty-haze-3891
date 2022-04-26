cab8935d6dd06d8f4fe69ea1fe1b4ed3874c70d0
craig cardwell <wizeduvcc@gmail.com>
2022-04-26 09:23:22 -0600

    add competition and team competition table, add models and model tests for relationships, add orderly gem, add migrations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ApplicationRecord
   has_many :players
+  has_many :team_competitions
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Team, type: :model do
-  it { should have_many :players }
+RSpec.describe Competition, type: :model do
   it { should have_many :team_competitions }
 end
